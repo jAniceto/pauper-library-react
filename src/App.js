26cd7934a528d63d81ddd54431e234a6a375eb67
jAniceto <jose.pedro.aniceto@gmail.com>
2020-05-08 18:26:38 +0100

    Added infinite scroll main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import './App.css';
 import Deck from './components/Deck';
@@ -15,23 +17,58 @@ class App extends React.Component {
     super(props);
     this.decks = decks;
     this.state = {
-      decks: this.decks
+      decks: this.decks.slice(0, 19), 
+      hasMoreDecks: true
+    }
+  }
+
+  loadItems(page) {
+    const decksPerSet = 10;
+    let currentDeckNumber = this.state.decks.length;
+    let newDeckSet = this.decks.slice(currentDeckNumber, currentDeckNumber + decksPerSet);
+    let decks = [...this.state.decks, ...newDeckSet];
+
+    if ((currentDeckNumber + decksPerSet) >= this.decks.length) {
+      this.setState({
+        decks: decks,
+        hasMoreDecks: false
+      })
+    } else {
+      this.setState({
+        decks: decks
+      })
     }
   }
 
   render() {
+    const loader = (
+      <div className="mt-5 text-center">
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </div>
+    );
+
     const deckCards = this.state.decks.map((deck, index) => 
       <Deck key={deck.name} stats={deck} />
     );
-
-    // console.log(this.decks[0]);
 
     return (
       <div className="App">
         <Header />
 
         <Container>
-          {deckCards}
+          {/* {deckCards} */}
+
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadItems.bind(this)}
+            hasMore={this.state.hasMoreDecks}
+            loader={loader}
+          >
+            {deckCards}
+          </InfiniteScroll>
+
         </Container>
 
         <Footer />
