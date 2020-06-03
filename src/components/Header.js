b6e2146b46004bc76756693e8717cec39156abf6
jAniceto <jose.pedro.aniceto@gmail.com>
2020-06-03 19:14:19 +0100

    Updated header links and clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,8 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import logo from '../logo-transparent.svg';
-import logoBig from '../pauper-library-logo-white.png';
 import './Header.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 
 class Header extends React.Component {
@@ -29,32 +25,15 @@ class Header extends React.Component {
             Pauper Deck Library
           </Navbar.Brand>
             <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/">Decks</Nav.Link>
+              {/* <Nav.Link as={Link} to="/index">Index</Nav.Link> */}
               <Nav.Link as={Link} to="/random">Randomize</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
+              <Nav.Link href="https://forms.gle/oD6FDZNVqd7MLWqq5" target="_blank">Suggest Decklist</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
 
-        {/* <Jumbotron fluid className="jumbotron bg-dark text-white">
-          <Container>
-            <Row>
-              <Col md={{ span: 9 }}>
-                <h1 className="mainTitle">Pauper Deck Library</h1>
-                <p className="subTitle text-muted">
-                  A curated collection of Pauper Decks.
-                </p>
-              </Col>
-              <Col md={{ span: 2, offset: 1 }}>
-                <img
-                  alt="logo"
-                  src={logoBig}
-                  className="d-inline-block align-top w-100"
-                />
-              </Col>
-
-            </Row>
-          </Container> 
-        </Jumbotron> */}
       </header>
     )
   }
