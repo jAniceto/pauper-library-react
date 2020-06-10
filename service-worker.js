d380e62564f4e520e277bd9a45bf0c93ac97fbce
jAniceto <jose.pedro.aniceto@gmail.com>
2020-06-10 19:55:32 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pauper-library-react/precache-manifest.553113544e4880b377e3b5b3dd286dd1.js"
+  "/pauper-library-react/precache-manifest.89d36257abd4233fece78ed04b8441c7.js"
 );
 
 self.addEventListener('message', (event) => {
